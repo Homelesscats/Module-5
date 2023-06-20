e0daec93a7bb23d273f3d3b3a2a043f38ca51b43
homelesscat <gilbertperezdt@gmail.com>
2023-06-20 13:11:33 -0700

    setting up current time

--- Develop/script.js
+++ Develop/script.js
@@ -3,7 +3,9 @@
 // in the html.
 $(document).ready(function () {
 
-
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage.
 // Listens for click events on any elements with class ".saveBtn".
 $(".saveBtn").on("click", function () {
   // Selects the sibling element using 'this' with class "description" of the current element textarea. 
@@ -23,14 +25,21 @@ $(".saveBtn").on("click", function () {
 
 
 
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+function hourTracker () {
+ //Need to pull current hour and time from dayjs:
+ let currentHour = day().hour()
+console.log(currentHour)
+$(".time-block").each(function() {
+// Uses parseInt and split on the block's id to get the number part of the string and store it in a variable.
+let blockTime = parseInt($(this).attr("id").split("-")[1]);
+
+
+
+
+
+}
+
+
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
